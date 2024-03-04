fe0e1f505a07eb999d6b908276a5604d504eca54
Amir Ghari <amir.ghari@metropolia.fi>
2024-03-04 12:36:37 +0200

    changes made ...

--- src/main/java/org/example/FarToCel.java
+++ src/main/java/org/example/FarToCel.java
@@ -1,6 +1,7 @@
 package org.example;
 public class FarToCel {
     public static void main(String[] args) {
+        System.out.print("FarToCel!");
     }
     public static float fahrToCel(float f) {
         float c = (float) ((f - 32) * 5 / 9.0);
@@ -13,4 +14,5 @@ public class FarToCel {
     public static float kelvinToFar(float kel) {
         return (float) ((kel - 273.15) * (9/5.0) + 32);
     }
+
 }
