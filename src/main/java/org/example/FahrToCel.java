57905708eca7521358d0393f8bd092f5fa6082b3
Amir Ghari <amir.ghari@metropolia.fi>
2024-03-04 12:08:00 +0200

    first_commit

---
+++ src/main/java/org/example/FahrToCel.java
@@ -0,0 +1,13 @@
+package org.example;
+public class FahrToCel {
+    public static void main(String[] args) {
+    }
+    public static float fahrToCel(float f) {
+        float c = (float) ((f - 32) * 5 / 9.0);
+        return Math.round(c);
+    }
+    public static float kelvinToCel(float kel) {
+
+        return (float) (kel - 273.15);
+    }
+}
